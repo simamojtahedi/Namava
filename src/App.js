f9f0475fd4adf9a2c2f25402eca0a5e4dd1abec5
sima <siilenus9@gmail.com>
2022-05-30 12:15:16 +0430

    App Routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      asas
-    </div>
+    <Router>
+      <Routes>
+          <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
