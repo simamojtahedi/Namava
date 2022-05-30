4e0cb4188dcf87b730c264a92a98bd6419d5deb5
sima <siilenus9@gmail.com>
2022-05-30 12:15:04 +0430

    Home page created

---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Container, Row } from 'react-bootstrap'
+import Slider from '../components/slider/Slider'
+
+const Home = () => {
+    return (
+        <Container fluid>
+            <Row>
+                <Slider />
+            </Row>
+        </Container>
+    )
+}
+
+export default Home
