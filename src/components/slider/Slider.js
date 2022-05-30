99315226221be076aa6cc57d55bf0b236cf11a7a
sima <siilenus9@gmail.com>
2022-05-30 12:14:53 +0430

    Slider components created

---
+++ src/components/slider/Slider.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import SliderItem from './SliderItem'
+import './slider.scss'
+
+const Slider = () => {
+    return (
+        <div className='col-12 slider'>
+            <SliderItem />
+        </div>
+    )
+}
+
+export default Slider
