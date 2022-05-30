7413cf5a347233a36bbeb4b0ae3b8307228ca55d
sima <siilenus9@gmail.com>
2022-05-30 15:21:12 +0430

    Context Providers added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
+import Providers from './context/Providers';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-          <Route path="/" element={<Home />} />
-      </Routes>
-    </Router>
+    <Providers>
+      <Router>
+        <Routes>
+            <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
+    </Providers>
   );
 }
 
