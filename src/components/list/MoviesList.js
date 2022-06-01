6efbe48e7d0305041a28186e1a49dbda70128cfd
sima <siilenus9@gmail.com>
2022-06-01 13:35:56 +0430

    MoviesList updated

--- src/components/list/MoviesList.js
+++ src/components/list/MoviesList.js
@@ -1,26 +1,45 @@
 import React, { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/css";
-import "swiper/css/pagination";
+import axios from "axios";
 import { Link } from "react-router-dom";
 import {BiChevronLeft} from 'react-icons/bi'
-import axios from "axios";
 import { imageUrl } from "../../utils/functions";
+import LazyLoad from 'react-lazyload';
+import "swiper/css/pagination";
+import "swiper/css";
 import './MoviesList.scss'
 
 const MoviesList = (props) => {
     const [items, setItems] = useState([])
 
-    const fetchData = async () => {
-        let {data} = await axios.get(`https://www.namava.ir/api/v1.0/post-groups/${props.data.payloadKey}/medias?pi=1&ps=20`)
-        if(data.succeeded) {
-            setItems(data.result)
-        }
-    }
-
     useEffect(() => {
-        fetchData()
-    }, [])
+        if(props.data && props.data.payloadType === "PostGroup") {
+            axios.get(`https://www.namava.ir/api/v1.0/post-groups/${props.data.payloadKey}/medias?pi=1&ps=20`)
+            .then(res => {
+                setItems(res.data.result)
+            })
+        } else if (props.data && props.data.payloadType === "Latest") {
+            axios.get(`https://www.namava.ir/api/v1.0/medias/latest?pi=1&ps=20`)
+            .then(res => {
+                setItems(res.data.result)
+            })
+        } else if (props.data && props.data.payloadType === "LatestEpisods") {
+            axios.get(`https://www.namava.ir/api/v1.0/medias/latest-episods?pi=1&ps=20`)
+            .then(res => {
+                setItems(res.data.result)
+            })
+        } else if (props.data && props.data.payloadType === "CategoryGroup") {
+            axios.get(`https://www.namava.ir/api/v1.0/category-groups/${props.data.payloadKey}/latest-medias?pi=1&ps=20`)
+            .then(res => {
+                setItems(res.data.result)
+            })
+        } else if (props.data && props.data.payloadType === "ExclusiveDubs") {
+            axios.get(`https://www.namava.ir/api/v1.0/medias/exclusive-dubs?pi=1&ps=20`)
+            .then(res => {
+                setItems(res.data.result)
+            })
+        }
+    }, [props.data])
 
     return (
         <div className='col-12 px-5 pt-2'>
@@ -37,10 +56,13 @@ const MoviesList = (props) => {
             >
                 {items.length > 0 && 
                     items.map(item => (
-                        <SwiperSlide key={item.id} >
+                        <SwiperSlide key={item.id || item.episodId} >
                             <Link to='/'>
-                                <img src={imageUrl(item.imageUrl)} />
-                                <h6>{item.caption}</h6>
+                                <LazyLoad className="image-placeholder" >
+                                    <img src={imageUrl(item.imageUrl || item.seriesImageUrl)} />
+                                </LazyLoad>
+                                <h6>{item.caption || item.seriesCaption }</h6>
+                                <span>{item.episodCaption?.split('-')[1]}</span>
                             </Link>
                         </SwiperSlide>
                     ))
