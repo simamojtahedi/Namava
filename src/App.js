0aa9cc13798adcfcae366bc7456d1f422d181a26
sima <siilenus9@gmail.com>
2022-06-03 13:31:52 +0430

    SinglePage route added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Providers from './context/Providers';
+import SinglePage from './pages/SinglePage';
 
 function App() {
   return (
     <Providers>
       <Router>
         <Routes>
-            <Route path="/" element={<Home />} />
+            <Route path={'/:type/:id'} element={<SinglePage />} />
+            <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
     </Providers>
