a9dd67380472a83e617b8002d722c6633ed9a664
sima <siilenus9@gmail.com>
2022-06-03 12:53:04 +0430

    getCoords function added

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,3 +1,25 @@
 export function imageUrl(url) {
     return `https://www.namava.ir${url}`
 }
+
+export function getCoords(element) {
+    setTimeout( () => {
+        const box = element.getBoundingClientRect()
+        const body = document.body;
+        const docEl = document.documentElement;
+    
+        const scrollTop = window.pageYOffset || docEl.scrollTop || body.scrollTop;
+        const scrollLeft = window.pageXOffset || docEl.scrollLeft || body.scrollLeft;
+    
+        const clientTop = docEl.clientTop || body.clientTop || 0
+        const clientLeft = docEl.clientLeft || body.clientLeft || 0
+    
+        const top = box.top + scrollTop - clientTop
+        const left = box.left + scrollLeft - clientLeft
+    
+        return {
+            top: Math.round(top), 
+            left: Math.round(left)
+        }
+    }, 20)
+}
