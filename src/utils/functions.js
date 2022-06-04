3faa2736aeb1d62274dc551d026d1afc2f49793b
sima <siilenus9@gmail.com>
2022-06-04 15:14:15 +0430

    getItemUrl added

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -23,3 +23,8 @@ export function getCoords(element) {
         }
     }, 20)
 }
+
+export function getItemUrl(title) {
+        const new_title = title.replace(/[^a-zA-Z0-9\u0633\u06A9\u06AF\u06C0\u06CC\u060C\u062A\u062B\u062C\u062D\u062E\u062F\u063A\u064A\u064B\u064C\u064D\u064E\u064F\u067E\u0670\u0686\u0698\u200C\u0621-\u0629\u0630-\u0639\u0641-\u0654]/g, '_');
+    return new_title;
+}
