20cb4b376517437401aa81eb3dd6e9a1e5a928dc
sima <siilenus9@gmail.com>
2022-06-05 22:00:39 +0430

    SinglePageTrailer created

---
+++ src/components/singlePage/SinglePageTrailer.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { imageUrl } from '../../utils/functions'
+import './singlePage.scss'
+
+const SinglePageTrailer = ({data}) => {
+    return (
+        <div className='single-page-trailers'>
+            <h5 className='mb-4'>تصاویر و جزئیات</h5>
+            <div>
+                {data?.slideImageList.length > 0 &&
+                    data.slideImageList.map(item => (
+                        <img src={`${imageUrl(item.url)}?anchor=middlecenter&crop=auto&scale=both&w=200&h=150`} />
+                    ))
+                }
+            </div>
+        </div>
+    )
+}
+
+export default SinglePageTrailer
