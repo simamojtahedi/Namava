db0b4be064c09c551e792a7b0470349636d33cee
sima <siilenus9@gmail.com>
2022-06-06 14:34:16 +0430

    Navbar added

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from './pages/Home';
 import Providers from './context/Providers';
 import SinglePage from './pages/SinglePage';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <Providers>
       <Router>
+        <Navbar />
         <Routes>
             <Route path={'/:type/:id'} element={<SinglePage />} />
             <Route path="/" exact element={<Home />} />
