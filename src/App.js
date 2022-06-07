5f6ca57300080e2a9e08d630b3cf29f0abd0d36e
sima <siilenus9@gmail.com>
2022-06-07 21:53:34 +0430

    routes updated

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ import SinglePage from './pages/SinglePage';
 import Navbar from './components/navbar/Navbar';
 import List from './pages/List';
 import Search from './pages/Search';
+import Collections from './pages/Collections';
+import "swiper/css/pagination";
+import "swiper/css";
+import ActorPage from './pages/ActorPage';
 
 function App() {
   return (
@@ -16,6 +20,9 @@ function App() {
             <Route path={'/:type/:id'} element={<SinglePage />} />
             <Route path="/list" element={ <List />} />
             <Route path="/search" element={<Search />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/collection-:id" element={<Collections />} />
+            <Route path="/person-:id" element={<ActorPage />} />
             <Route path="/" exact element={ <Home />} />
         </Routes>
       </Router>
