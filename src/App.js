44faa6a8d7faf70efd476ee0bfbe641a1ced77f9
sima <siilenus9@gmail.com>
2022-06-07 20:11:39 +0430

    routes updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Providers from './context/Providers';
 import SinglePage from './pages/SinglePage';
 import Navbar from './components/navbar/Navbar';
+import List from './pages/List';
 import Search from './pages/Search';
 
 function App() {
@@ -13,8 +14,9 @@ function App() {
         <Navbar />
         <Routes>
             <Route path={'/:type/:id'} element={<SinglePage />} />
-            <Route path="/search" exact element={<Search />} />
-            <Route path="/" exact element={<Home />} />
+            <Route path="/list" element={ <List />} />
+            <Route path="/search" element={<Search />} />
+            <Route path="/" exact element={ <Home />} />
         </Routes>
       </Router>
     </Providers>
