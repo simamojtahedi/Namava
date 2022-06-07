9d80525251bb29c6e4239c221fabb9454f47742c
sima <siilenus9@gmail.com>
2022-06-07 13:55:10 +0430

    search page route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Providers from './context/Providers';
 import SinglePage from './pages/SinglePage';
 import Navbar from './components/navbar/Navbar';
+import Search from './pages/Search';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Navbar />
         <Routes>
             <Route path={'/:type/:id'} element={<SinglePage />} />
+            <Route path="/search" exact element={<Search />} />
             <Route path="/" exact element={<Home />} />
         </Routes>
       </Router>
